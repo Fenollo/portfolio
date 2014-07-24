bc29d05beac92d1bb335714c927154d3c7b27ea1
Fenollo <euroveto@mac.com>
2014-07-24 14:49:30 +0200

    up

--- meta/menu.js
+++ meta/menu.js
@@ -1,37 +1,37 @@
 var menu = " \
     <div id=\"menu\" data-markdown> \
-      # [MATHILDE FENOLL](/) \
+      # [GILLES FENOLL](/) \
       <br/> \
+      ##### 2014 \
+      * [](/.htm) \
       ##### 2013 \
-      * [Malacologie](/malacologie.htm) \
-      ##### 2012 \
-      * [apparition](/clients/apparition/) \
-      * [balai bois](/clients/balai-bois/) \
+      * [](///) \
+      * [](/clients/balai-bois/) \
       * [balai gaz](/clients/balai-gaz/) \
       * [crânes brûlés](/clients/cranes-brules/) \
       * [en bas de l'échelle](/clients/en-bas-de-lechelle/) \
-      ##### 2011 \
+      ##### 2012 \
       * [Ninfas Méduas](/clients/ninfas-meduas/) \
       * [épicentre](/clients/epicentre/) \
       * [périmètre(s)](/clients/perimetres/) \
       * [crâne image](/clients/crane-image/) \
       * [crânes béton](/clients/cranes-beton/) \
-      ##### 2010 \
+      ##### 2011 \
       * [images de fer](/clients/images-de-fer/) \
       * [nature piquée](/clients/nature-pique/) \
       * [dispersion](/clients/projet-de-fer/) \
       ##### Texte \
-      * [J'écris](/project/jecris/) \
-      * [Percer, crever, éclater](/project/percer-crever-eclater/) \
-      ##### image d'expo \
-      * [MONDEMON](/image-dexpo/mondemon/) \
-      * [Nuit des musées](/clients/nuit-des-musees/) \
-      * [Après Nicolas](/clients/apres-nicolas/) \
-      * [Esads, lieux d'expositions](/image-dexpo/dnsep/) \
+      * [](///) \
+      * [](/project//) \
+      ##### images \
+      * [MONDEMON](//) \
+      * [Nuit des musées](/clients//) \
+      * [](/clients//) \
+      * [](/) \
       ##### Info \
       * [contact](/contact.htm) \
       * [home](/) \
-      * [biographie](/clients/sony/) \
+      * [](/clients//) \
       * [liens](/liens.htm) \
     </div> \
 ";
